0e9750fedd1e4c08420f858558a4d27474ef127a
Fabio Andre Arredondo Mendieta <fabioarredondo44@gmail.com>
2024-04-24 23:02:10 -0400

    first commit

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const cors = require('cors');
 
   app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:6657',
     credentials: true,
   }));
   await app.listen(3001); //Puerto del Backend
