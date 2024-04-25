d40ea45c0a7e3b8f00dd5b9d7053f5e0f78f5c75
Fabio Andre Arredondo Mendieta <fabioarredondo44@gmail.com>
2024-04-25 01:19:28 -0400

    first commit

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const cors = require('cors');
 
   app.use(cors({
-    origin: 'https://frontendsw1-production.up.railway.app/',
+    origin: 'https://frontendsw1-production.up.railway.app',
     credentials: true,
   }));
   await app.listen(3001); //Puerto del Backend
